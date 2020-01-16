758ba1c9fb895e410edc3562f544bb496a7dd14a
OhBoyItsFrancis <francis.chacko2002@gmail.com>
2020-01-15 20:05:19 -0500

    skrt skrt finally got it to turn fast (and correct...yay for pid)

--- src/main/java/frc/robot/autonomous/Win.java
+++ src/main/java/frc/robot/autonomous/Win.java
@@ -7,11 +7,15 @@ public class Win extends GenericAutonomous {
 
       double startingYaw      = 0.0;
       double startingDistance = 0.0;
-      PIDModule PIDSteering = new PIDModule(0.06, 1.0e-3, 1.0e-4);
+      PIDModule PIDSteering = new PIDModule(1.0e-2, 1.0e-4, 0.0e-4);
       double correction;
+      static double currentYaw = 0;
+
+      @Override public void autonomousInit(GenericRobot robot) {
+            robot.resetAttitude();
+      }
 
       @Override public void autonomousPeriodic(GenericRobot robot) {
-            double currentYaw = 0;
             double currentDistance = 0;
             switch (autonomousStep) {
                   case 0:
@@ -23,21 +27,23 @@ public class Win extends GenericAutonomous {
                         startingYaw = robot.getYaw();
                         autonomousStep = 3;
                   case 3:
-                        robot.driveLeftInPlace(0.2);
+                        robot.driveLeftInPlace(0.4);
                         currentYaw = robot.getYaw();
                         if (currentYaw - startingYaw < -90) {
                               robot.driveForward(0);
-                              currentYaw = robot.getYaw();
                               autonomousStep = 4;
                         } else break;
                   case 4:
                         startingDistance = robot.getDistanceInchesLeft();
-                        PIDSteering.resetError();
+                        PIDSteering.resetError(); //reset pid stuff
+                        currentYaw = -90;
+                        // currentYaw = robot.getYaw();
                         autonomousStep = 5;
+                        break;
                   case 5:
                         PIDSteering.setHeading(robot.getYaw()-currentYaw);
                         correction = PIDSteering.getCorrection();
-                        robot.setMotorPowerPercentage(0.2*(1+correction), 0.2*(1-correction));
+                        robot.setMotorPowerPercentage(0.4*(1+correction), 0.4*(1-correction));
                         currentDistance = robot.getDistanceInchesLeft();
                         if (currentDistance - startingDistance > 66.91) {
                               robot.driveForward(0);
@@ -47,17 +53,24 @@ public class Win extends GenericAutonomous {
                         startingYaw = robot.getYaw();
                         autonomousStep = 7;
                   case 7:
-                        robot.driveRightInPlace(0.2);
+                        robot.driveRightInPlace(0.4);
                         currentYaw = robot.getYaw();
-                        if (currentYaw - startingYaw > 90) {
+                        if (currentYaw - startingYaw > 85) {
                               robot.driveForward(0);
                               autonomousStep = 8;
                         } else break;
                   case 8:
                         startingDistance = robot.getDistanceInchesLeft();
+                        PIDSteering.resetError();
+                        currentYaw = 0;
+                        // currentYaw = robot.getYaw();
                         autonomousStep = 9;
+
                   case 9:
-                        robot.driveForward(0.2);
+                        PIDSteering.setHeading(robot.getYaw() - currentYaw);
+                        correction = PIDSteering.getCorrection();
+                        robot.setMotorPowerPercentage(0.4*(1+correction), 0.4*(1-correction));
+                        //robot.driveForward(0.2);
                         currentDistance = robot.getDistanceInchesLeft();
                         if (currentDistance - startingDistance > 195) {
                               robot.driveForward(0);
