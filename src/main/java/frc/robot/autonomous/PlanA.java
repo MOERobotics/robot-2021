95dc1c37b97adb4464a6befd5dab1540c5971927
Louminator <louisfrossi@gmail.com>
2020-01-26 22:31:33 -0500

    A little more de-crappification.  (Removal of commented code and some Smart Dashboard calls.)

--- src/main/java/frc/robot/autonomous/PlanA.java
+++ src/main/java/frc/robot/autonomous/PlanA.java
@@ -42,7 +42,7 @@ public class PlanA extends GenericAutonomous {
                         autonomousStep = 1;
                         break;
                   case 1:
-                        PIDSteering.setHeading(robot.getDistanceInchesLeft()/robot.getDistanceInchesRight()-0.5); //-2 is A
+                        PIDSteering.sendError(robot.getDistanceInchesLeft() / robot.getDistanceInchesRight() - 0.5); //-2 is A
                         correction = PIDSteering.getCorrection();
                         robot.setMotorPowerPercentage((defaultSpeed * .75) * (1 + correction), (defaultSpeed * 1.5) * (1 - correction));
                         currentDistance = robot.getDistanceInchesRight();
@@ -56,19 +56,13 @@ public class PlanA extends GenericAutonomous {
                         startingYaw = robot.getYaw();
                         autonomousStep = 3;
                   case 3:
-                        PIDSteering.setHeading(robot.getDistanceInchesLeft()/robot.getDistanceInchesRight()-2.0);
+                        PIDSteering.sendError(robot.getDistanceInchesLeft() / robot.getDistanceInchesRight() - 2.0);
                         correction = PIDSteering.getCorrection();
                         robot.setMotorPowerPercentage((defaultSpeed * 1.5) * (1 + correction), (defaultSpeed * .75) * (1 - correction));
                         currentDistance = robot.getDistanceInchesLeft();
-                        if(currentDistance - startingDistance > outerArc) {
+                        if (currentDistance - startingDistance > outerArc) {
                               autonomousStep = 4;
                         }
-
-                        SmartDashboard.putNumber("Pid heading", outerRadius * yawDifference - (robot.getDistanceInchesLeft() - startingDistance));
-                        SmartDashboard.putNumber("startingYaw", startingYaw);
-                        SmartDashboard.putNumber("yawError", yawError); //robot.getYaw() - startingYaw);
-
-
                         break;
                   case 4: //PID reset for straightaway
                         startingDistance = robot.getDistanceInchesLeft();
@@ -76,9 +70,9 @@ public class PlanA extends GenericAutonomous {
                         currentYaw = 0;
                         autonomousStep = 5;
                   case 5:
-                        PIDSteering.setHeading(robot.getYaw() - currentYaw);
+                        PIDSteering.sendError(robot.getYaw() - currentYaw);
                         correction = PIDSteering.getCorrection();
-                        robot.setMotorPowerPercentage(defaultSpeed *(1+correction), defaultSpeed *(1-correction));
+                        robot.setMotorPowerPercentage(defaultSpeed * (1 + correction), defaultSpeed * (1 - correction));
                         currentDistance = robot.getDistanceInchesLeft();
                         if (currentDistance - startingDistance > 100) { //maybe change depending on how far we need to go
                               robot.driveForward(0);
@@ -90,49 +84,7 @@ public class PlanA extends GenericAutonomous {
                         break;
 
             }
-
-                  /*
-                             case 0:                             //shorten steps to implement S curve B)
-                        if (true) autonomousStep = 2;
-                        break;
-                  case 1:
-                  case 2:
-                        startingYaw = robot.getYaw();
-                        autonomousStep = 3;
-                  case 3:
-                        robot.driveLeftInPlace(defaultSpeed);
-                        currentYaw = robot.getYaw();
-                        if (currentYaw - startingYaw < -90) {
-                              robot.driveForward(0);
-                              autonomousStep = 4;
-                        } else break;
-                  case 4:
-                        startingDistance = robot.getDistanceInchesLeft();
-                        PIDSteering.resetError(); //reset pid stuff
-                        currentYaw = -90;
-                        autonomousStep = 5;
-                        break;
-                  case 5: //straightaway, a little bit of oscillation, may need to turn P & D - PID coefficients
-                        PIDSteering.setHeading(robot.getYaw() - currentYaw);
-                        correction = PIDSteering.getCorrection();
-                        robot.setMotorPowerPercentage(1.5 * defaultSpeed * (1 + correction), 1.5 * defaultSpeed * (1 - correction));
-                        currentDistance = robot.getDistanceInchesLeft();
-                        if (currentDistance - startingDistance > 100) {
-                              robot.driveForward(0);
-                              autonomousStep = 6;
-                        }
-                        break;
-                  case 6: //cease your autnomous
-                        robot.driveForward(0);
-                        //                               ¯\_(ツ)_/¯
-                        break;
-
-            }
-            SmartDashboard.putNumber("Correction",correction);
-      }
-}
-
-
+      }}
 
 /*
 
