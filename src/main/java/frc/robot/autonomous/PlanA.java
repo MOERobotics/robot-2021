ac0818be5d7883aa2cc567d112c56e6c3be13b7c
Riley <rilegray21@ncs.charter.k12.de.us>
2020-02-05 19:54:31 -0500

    deceleration added

--- src/main/java/frc/robot/autonomous/PlanA.java
+++ src/main/java/frc/robot/autonomous/PlanA.java
@@ -30,8 +30,7 @@ public class PlanA extends GenericAutonomous {
             double currentDistance = 0;
             double yawError;
             switch (autonomousStep) {
-
-                  case -1: //resets everything and waits
+                  case -1:
                         robot.resetAttitude();
                         robot.resetEncoders();
                         if (System.currentTimeMillis() >= startingTime + 100) {
@@ -39,68 +38,85 @@ public class PlanA extends GenericAutonomous {
                         }
                         break;
 
-                  case 0: //PID reset for 1st (left) arc
+
+                  case 0:
+
+                        autonomousStep = 1;
+                        break;
+
+                  case 1: //resets everything and waits
+                       autonomousStep = 2;
+                       break;
+
+                  case 2: //PID reset for 1st (left) arc
                         PIDSteering.reset();
                         PIDSteering.disableContinuousInput();
                         startingYaw = robot.getYaw();
                         startingDistance = robot.getDistanceInchesRight();
-                        autonomousStep = 1;
+                        autonomousStep = 3;
                         break;
 
-                  case 1: //1st (left) arc
+                  case 3: //1st (left) arc
                         yawDifference = continuousAngleDiff((robot.getYaw() - startingYaw) / 180 * Math.PI);
                         correction = PIDSteering.calculate((robot.getDistanceInchesRight() - startingDistance) + outerRadius * yawDifference);
                         SmartDashboard.putNumber("Pid heading", (robot.getDistanceInchesRight() - startingDistance) + outerRadius * yawDifference);
                         robot.setMotorPowerPercentage((defaultSpeed * .75) * (1 + correction), (defaultSpeed * 1.5) * (1 - correction));
                         currentDistance = robot.getDistanceInchesRight();
                         if (currentDistance - startingDistance > outerArcLength) {
-                              autonomousStep = 2;
+                              autonomousStep = 4;
                         }
                         break;
 
-                  case 2: //PID reset for 2nd (right) arc
+                  case 4: //PID reset for 2nd (right) arc
                         PIDSteering.reset();
                         PIDSteering.disableContinuousInput();
                         startingDistance = robot.getDistanceInchesLeft();
                         startingYaw = robot.getYaw();
-                        autonomousStep = 3;
+                        autonomousStep = 5;
                         break;
 
-                  case 3: //2nd (right) arc
+                  case 5: //2nd (right) arc
                         yawDifference = continuousAngleDiff((robot.getYaw() - startingYaw)*Math.PI/180.0);
                         correction = PIDSteering.calculate(outerRadius * yawDifference - (robot.getDistanceInchesLeft() - startingDistance));
                         robot.setMotorPowerPercentage((defaultSpeed * 1.5) * (1 + correction), (defaultSpeed * .75) * (1 - correction));
                         currentDistance = robot.getDistanceInchesLeft();
                         if(currentDistance - startingDistance > outerArcLength) {
-                              autonomousStep = 4;
+                              autonomousStep = 6;
                         }
                         break;
 
-                  case 4: //PID reset for straightaway
+                  case 6: //PID reset for straightaway
                         startingDistance = robot.getDistanceInchesLeft();
                         PIDSteering.reset();
                         PIDSteering.enableContinuousInput(-180,180);
                         currentYaw = 0;
-                        autonomousStep = 5;
+                        autonomousStep = 7;
                         break;
 
-                  case 5: //straightaway, a little bit of oscillation, may need to turn P & D - PID coefficients
+                  case 7: //straightaway, a little bit of oscillation, may need to turn P & D - PID coefficients
                         correction = PIDSteering.calculate(robot.getYaw() - currentYaw);
                         robot.setMotorPowerPercentage(1.5 * defaultSpeed * (1 + correction), 1.5 * defaultSpeed * (1 - correction));
                         currentDistance = robot.getDistanceInchesLeft();
                         //decrescendo power
                         if(currentDistance - startingDistance > 45){ //start to decrement?
-                              autonomousStep = 6;
+                              autonomousStep = 8;
 
                         }
-
-
-                  case 6: //decrement power
-                        double slowToStop = (defaultSpeed - (defaultSpeed / 15)*((currentDistance-startingDistance) - 45)); //?
-                        autonomousStep = 7;
                         break;
 
-                  case 7: //cease your autnomous
+
+                  case 8: //decrement power
+                        currentDistance = robot.getDistanceInchesLeft();
+                        double slowToStop = (defaultSpeed - (defaultSpeed / 15)*((currentDistance-startingDistance)-45)); //?
+                        correction = PIDSteering.calculate(robot.getYaw() - currentYaw);
+                        robot.setMotorPowerPercentage(1.5 * slowToStop * (1 + correction), 1.5 * slowToStop * (1 - correction)); // div by 2 to debug
+                        if(currentDistance - startingDistance > 60){
+                              autonomousStep = 9;
+
+                        }
+                        break;
+
+                  case 9: //cease your autnomous
                         robot.driveForward(0);
                         //                               ¯\_(ツ)_/¯
                         break;
