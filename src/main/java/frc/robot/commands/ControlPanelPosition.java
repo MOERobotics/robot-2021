1a53ff3e08335b1f61e18e767c9c26dbaec4c3de
zollers <zollers375@gmail.com>
2020-02-17 18:29:56 -0500

    Yes Kevin I finally commited.

--- src/main/java/frc/robot/commands/ControlPanelPosition.java
+++ src/main/java/frc/robot/commands/ControlPanelPosition.java
@@ -1,11 +1,36 @@
 package frc.robot.commands;
 
 import frc.robot.genericrobot.GenericRobot;
+import edu.wpi.first.wpilibj.DriverStation;
 
 public class ControlPanelPosition extends GenericCommand{
 
     @Override
     public void step(GenericRobot robot){
-
+        String gameData;
+        gameData = DriverStation.getInstance().getGameSpecificMessage();
+        if(gameData.length() > 0)
+        {
+            switch (gameData.charAt(0))
+            {
+                case 'B' :
+                    //Blue case code
+                    break;
+                case 'G' :
+                    //Green case code
+                    break;
+                case 'R' :
+                    //Red case code
+                    break;
+                case 'Y' :
+                    //Yellow case code
+                    break;
+                default :
+                    //This is corrupt data
+                    break;
+            }
+        } else {
+            //Code for no data received yet
+        }
     }
 }
