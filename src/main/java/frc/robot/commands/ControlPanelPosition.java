bf33c51b9638ca02dcd270904793b0eedf92b32e
zollers <zollers375@gmail.com>
2020-02-17 18:39:48 -0500

    Yes Kevin I finally commited.

--- src/main/java/frc/robot/commands/ControlPanelPosition.java
+++ src/main/java/frc/robot/commands/ControlPanelPosition.java
@@ -1,14 +1,20 @@
 package frc.robot.commands;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel;
+import edu.wpi.first.wpilibj.util.Color;
 import frc.robot.genericrobot.GenericRobot;
 import edu.wpi.first.wpilibj.DriverStation;
+import frc.robot.genericrobot.WheelOfFortune;
 
 public class ControlPanelPosition extends GenericCommand{
 
-
+    CANSparkMax spinner = new CANSparkMax(99, CANSparkMaxLowLevel.MotorType.kBrushless);
+    WheelOfFortune controlPanel = new WheelOfFortune();
+    Color endColor = new WheelOfFortune.GoodColor(0, 0, 0, "Default");
 
     @Override
-    public void step(GenericRobot robot){
+    public void begin(GenericRobot robot){
         String gameData;
         gameData = DriverStation.getInstance().getGameSpecificMessage();
         if(gameData.length() > 0)
@@ -17,15 +23,19 @@ public class ControlPanelPosition extends GenericCommand{
             {
                 case 'B' :
                     //Blue case code
+                    endColor = WheelOfFortune.kBlueTarget;
                     break;
                 case 'G' :
                     //Green case code
+                    endColor = WheelOfFortune.kGreenTarget;
                     break;
                 case 'R' :
                     //Red case code
+                    endColor = WheelOfFortune.kRedTarget;
                     break;
                 case 'Y' :
                     //Yellow case code
+                    endColor = WheelOfFortune.kYellowTarget;
                     break;
                 default :
                     //This is corrupt data
@@ -34,5 +44,10 @@ public class ControlPanelPosition extends GenericCommand{
         } else {
             //Code for no data received yet
         }
+    }
+
+    @Override
+    public void step(GenericRobot robot){
+
     }
 }
