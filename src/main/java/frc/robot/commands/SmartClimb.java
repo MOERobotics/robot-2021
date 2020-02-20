634576ccfac54dde198c1b4b8304ce68562dedad
Riley <rilegray21@ncs.charter.k12.de.us>
2020-02-20 18:11:17 -0500

    basic class setup

---
+++ src/main/java/frc/robot/commands/SmartClimb.java
@@ -0,0 +1,33 @@
+package frc.robot.commands;
+
+import frc.robot.genericrobot.GenericRobot;
+
+public class SmartClimb extends GenericCommand{
+    /*
+    double leftPower;
+    double rightPower;
+    private boolean aligning;
+    double currentDistance = 0;
+    double setPoint;
+    double setPointDeadzone;
+    double constant;
+     */
+    double currentRoll;
+
+
+
+    public SmartClimb(){
+
+    }
+    @Override
+    public void begin(GenericRobot robot) {
+
+    }
+
+    @Override
+    public void step(GenericRobot robot) {
+
+    }
+
+
+}
