7ea76aec09fc5ce2ec248de2ea262c3c8ad684a1
Louminator <louisfrossi@gmail.com>
2021-03-10 20:20:01 -0500

    Fixed shooter presets

--- src/main/java/frc/robot/genericrobot/Falcon.java
+++ src/main/java/frc/robot/genericrobot/Falcon.java
@@ -231,12 +231,10 @@ public class Falcon extends GenericRobot{
 
     private static final ShooterSpeedPreset
             SHOOTER_SPEED_OFF   = new ShooterSpeedPreset(   0,    0),
-            SHOOTER_SPEED_SHORT = new ShooterSpeedPreset(2400, 2400),
-            //SHOOTER_SPEED_SHORT = new ShooterSpeedPreset(2285, 2285),
+            SHOOTER_SPEED_SHORT = new ShooterSpeedPreset(2285, 2285),
             SHOOTER_SPEED_MID   = new ShooterSpeedPreset(2620, 2620),
             SHOOTER_SPEED_LONG  = new ShooterSpeedPreset(4000, 3000), //not final
-            SHOOTER_SPEED_YEET  = new ShooterSpeedPreset(2495, 2495);
-            //SHOOTER_SPEED_YEET  = new ShooterSpeedPreset(5000, 5000);
+            SHOOTER_SPEED_YEET  = new ShooterSpeedPreset(5000, 5000);
 
 
 
