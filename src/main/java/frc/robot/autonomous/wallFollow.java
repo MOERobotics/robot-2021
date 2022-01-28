bf24b1854a8af90466e1a414d60cfbe5347f19e9
Malti <maltimjohn1@gmail.com>
2022-01-27 20:30:18 -0500

    Wall-follow code. robot spins around. Need to troubleshoot. Lidars are goofy.

--- src/main/java/frc/robot/autonomous/wallFollow.java
+++ src/main/java/frc/robot/autonomous/wallFollow.java
@@ -6,5 +6,66 @@ import frc.robot.genericrobot.GenericRobot;
 
 public class wallFollow extends GenericAutonomous{
 
-    public void autonomousInit(GenericRobot Robot){}
+    double x_one;
+    double x_two;
+
+    double theta;
+    double LidarGap = 9.25;
+
+    double GoldOut = 14;
+    double GoldIn = 10;
+
+    boolean start = true;
+
+    double defaultPower = .2;
+    double rightPower;
+    double leftPower;
+
+    double correction;
+
+
+
+
+
+    public void autonomousInit(GenericRobot robot){
+        start = true;
+
+    }
+
+    public void autonomousPeriodic(GenericRobot robot){
+        PIDController PIDSteering = new PIDController(robot.getPIDmaneuverP(), robot.getPIDmaneuverI(), robot.getPIDmaneuverD());
+        SmartDashboard.putNumber("theta", theta);
+
+        x_one = robot.getLidarDistanceInchesFront();
+        x_two = robot.getLidarDistanceInchesLeft();
+
+        double x_avg = (x_one+x_two)/2;
+
+        theta = Math.toDegrees(Math.atan((x_two-x_one)/ LidarGap));
+
+        if (start){
+            PIDSteering.reset();
+            PIDSteering.enableContinuousInput(-180, 180);
+            start = false;
+        }
+
+        if (x_avg < GoldOut && x_avg > GoldIn){
+            correction = PIDSteering.calculate(theta);
+            leftPower = defaultPower + correction;
+            rightPower = defaultPower - correction;
+        }
+
+        if (x_avg > GoldOut){
+            correction = PIDSteering.calculate(theta-20);
+            leftPower = defaultPower + correction;
+            rightPower = defaultPower - correction;
+        }
+
+        if (x_avg < GoldIn){
+            correction = PIDSteering.calculate(theta+20);
+            leftPower = defaultPower + correction;
+            rightPower = defaultPower - correction;
+        }
+        robot.setMotorPowerPercentage(leftPower,rightPower);
+    }
 }
